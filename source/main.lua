8d2e3dd0bb80cc36068679ef3dc624c50d93d854
Alex Kotliarov <slicer256@gmail.com>
2024-11-25 21:47:48 +0100

    Add progressbar

--- source/main.lua
+++ source/main.lua
@@ -9,7 +9,17 @@ local fontClock = gfx.font.new("fonts/Mikodacs-Clock")
 local isRunning = false
 local startTime = nil
 local elapsedTime = 0
-local recordedTimes = {}
+local recordedTimes = {
+    --     {
+    --     ["elapsed"] = 0,
+    --     ["start"] = 785869484,
+    --     ["end"] = 785875484
+    -- }, {
+    --     ["elapsed"] = 0,
+    --     ["start"] = 785838284,
+    --     ["end"] = 785849084
+    -- }
+}
 
 local function secondsToTime(seconds)
     local minutes = math.floor(seconds / 60)
@@ -34,7 +44,25 @@ local function toggleStopwatch()
         elapsedTime = 0
         startTime = playdate.getSecondsSinceEpoch()
         isRunning = true
+        print("Start time: " .. startTime)
     end
+end
+
+local function drawProgressbar()
+    local pixelsPerSecond<const> = 400 / 60 / 60 / 24
+    local midnightTimestamp = playdate.getSecondsSinceEpoch() - playdate.getTime().hour * 3600 -
+                                  playdate.getTime().minute * 60 - playdate.getTime().second
+    for i, record in ipairs(recordedTimes) do
+        local barX = (record.start - midnightTimestamp) * pixelsPerSecond;
+        local barWidth = (record["end"] - record["start"]) * pixelsPerSecond;
+        if barWidth < 3 then
+            barWidth = 3
+        end
+        gfx.setColor(gfx.kColorBlack)
+        gfx.fillRect(barX, 240 - 20, barWidth, 20)
+    end
+    gfx.setLineWidth(3)
+    gfx.drawLine(0, 240 - 20, 400, 240 - 20)
 end
 
 local function updateScreen()
@@ -50,11 +78,12 @@ local function updateScreen()
     if isRunning then
         local displayTime = (playdate.getSecondsSinceEpoch() - startTime)
         gfx.setFont(fontClock)
-        gfx.drawText(secondsToTime(displayTime), 10, 100)
+        gfx.drawText(secondsToTime(displayTime), 60, 100)
     else
         gfx.drawText("Press A to start working", 10, 100)
     end
 
+    drawProgressbar()
 end
 
 function playdate.update()
