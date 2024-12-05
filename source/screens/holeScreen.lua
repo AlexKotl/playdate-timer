ea087e53c3cb2ba70704a849875803a3ce972b78
Alex Kotliarov <slicer256@gmail.com>
2024-12-05 15:38:28 +0100

    Baseline for menu

--- source/screens/holeScreen.lua
+++ source/screens/holeScreen.lua
@@ -20,14 +20,14 @@ local holeItems<const> = {
         title = "Small carpet",
         category = "carpet",
         description = "Carpet. It's small. It smells. It's carpet.",
-        price = 0,
+        price = 20,
         image = gfx.image.new("assets/hole/carpet1")
     },
     desk1 = {
         title = "Small desk",
         category = "desk",
         description = "Almost fits for work.",
-        price = 0,
+        price = 100,
         image = gfx.image.new("assets/hole/desk1")
     },
     windows1 = {
@@ -39,9 +39,32 @@ local holeItems<const> = {
     }
 }
 local currentItems = {"hole1", "couch1", "carpet1", "desk1", "windows1"}
+local currentBalance = 0
+local isMenuVisible = true
+local selectedMenuItem = 1
+
+local function drawMenu()
+    local itemHeight = 40
+
+    gfx.setColor(1)
+    gfx.fillRect(10, 10, 220, 220)
+    gfx.setColor(0)
+    gfx.drawRect(10, 10, 220, 220)
+
+    -- for each values holeItems
+
+    local i = 0
+    for key, item in pairs(holeItems) do
+
+        gfx.drawText(item.title, 20, 20 + (i * itemHeight))
+        -- gfx.drawText(item.description, 20, 40 + (i * itemHeight))
+        -- gfx.drawText("Price: $" .. item.price, 20, 60 + (i * itemHeight))
+        i = i + 1
+    end
+end
 
 function HoleScreen:show()
-
+    currentBalance = 200
 end
 
 function HoleScreen:hide()
@@ -57,7 +80,11 @@ function HoleScreen:update()
         end
     end
 
-    gfx.drawText("Hole", 20, 20)
+    gfx.drawText("You have: $" .. currentBalance, 100, 10)
+
+    if isMenuVisible then
+        drawMenu()
+    end
 end
 
 function HoleScreen.downButtonDown()
